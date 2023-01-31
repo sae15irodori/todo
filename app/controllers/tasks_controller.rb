90b22fce0bcb36a0b735fc4cbf1d0eaa8c23b6cf
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-31 07:16:19 +0000

    Add create,edit,update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,34 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+  
+  def edit
+    @task = get_id
+  end
+  
+  def update
+    task = get_id
+    task.update(task_params)
+    redirect_to tasks_path
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:title)
+  end
+  
+  def get_id
+    Task.find(params[:id])
+  end
 end
