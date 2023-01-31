8e31629b9a0d091f50b3e9619cbdc2bc8ae8f44e
EC2 Default User <ec2-user@ip-172-31-46-99.ap-northeast-1.compute.internal>
2023-01-31 07:22:09 +0000

    Add destroy機能

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,13 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
   
+  def destroy
+    @task = get_id
+    @task.destroy
+    redirect_to tasks_path
+  end
+  
+  
   private
   
   def task_params
